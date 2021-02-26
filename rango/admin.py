d05c5bbe4f65474c96f4a377f5baf10982d879ad
shubham suroshe <shubham.Suroshe1@gmail.com>
2021-02-26 17:28:19 +0530

    chapter 6

--- rango/admin.py
+++ rango/admin.py
@@ -5,6 +5,9 @@ from rango.models import Category, Page
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title','category','url')
 
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
 
 admin.site.register(Category)
 admin.site.register(Page, PageAdmin)
--- rango/views.py
+++ rango/views.py
@@ -2,12 +2,30 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from rango.models import Category
+from rango.models import Page
 
 
 def index(request):
+    context_dict = {}
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories':category_list}
+    most_viwed_page = Page.objects.order_by('-views')[:5]
+    context_dict['categories'] = category_list
+    context_dict['most_viwed_pages'] = most_viwed_page
     return render(request, 'rango/index.html', context_dict)
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request,'rango/category.html', context_dict)
 
 def about(request):
     return HttpResponse("Rango says hey there its about page")
